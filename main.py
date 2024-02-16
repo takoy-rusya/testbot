b0b828169c7c0110ead96e1ed5130cb65470e401
Руслан Ефимов <ruskus011294@gmail.com>
2024-02-16 23:32:18 +0300

    Добавил GET-запросы для добавления собачек и лисичек

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import requests
 import time
+from random import choice
 
 API_URL = 'https://api.telegram.org/bot'
 API_CAT_URL = 'https://api.thecatapi.com/v1/images/search'
+API_DOG_URL = 'https://random.dog/woof.json'
+API_FOX_URL = ' https://randomfox.ca/floof/'
 BOT_TOKEN = '6460540408:AAHkYNEg2skgqOBKKhHjfAacLjMq-V4tdc0'
 ERROR_TEXT = 'Здесь должны были быть котики'
 MAX_COUNTER = 100
+api_url = [API_DOG_URL, API_FOX_URL, API_CAT_URL]
 
 offset = -2
 counter = 0
-cat_response = requests.Response
-cat_link = str
+response = requests.Response
+link = str
 
 while counter < MAX_COUNTER:
     print('attempt=', counter)
+    random_url = choice(api_url)
 
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
@@ -21,10 +26,16 @@ while counter < MAX_COUNTER:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CAT_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            response = requests.get(random_url)
+            if response.status_code == 200 and random_url == API_CAT_URL:
+                link = response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={link}')
+            elif response.status_code == 200 and random_url == API_FOX_URL:
+                link = response.json()['image']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={link}')
+            elif response.status_code == 200 and random_url == API_DOG_URL:
+                link = response.json()['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={link}')
             else:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
