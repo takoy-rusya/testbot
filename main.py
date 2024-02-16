bc577532f7a5645f853e4528ea802fa5cddca0f6
Руслан Ефимов <ruskus011294@gmail.com>
2024-02-16 22:54:00 +0300

    Добавлена загрузка котиков при апдейте по GET-запросу

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 import time
 
 API_URL = 'https://api.telegram.org/bot'
+API_CAT_URL = 'https://api.thecatapi.com/v1/images/search'
 BOT_TOKEN = '6460540408:AAHkYNEg2skgqOBKKhHjfAacLjMq-V4tdc0'
-TEXT = 'Я получил апдейт! Я не бесполезен!Олололоо!)!)0)!011'
+ERROR_TEXT = 'Здесь должны были быть котики'
 MAX_COUNTER = 100
 
 offset = -2
 counter = 0
-chat_id = int
+cat_response = requests.Response
+cat_link = str
 
 while counter < MAX_COUNTER:
     print('attempt=', counter)
@@ -19,7 +21,12 @@ while counter < MAX_COUNTER:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            cat_response = requests.get(API_CAT_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1
