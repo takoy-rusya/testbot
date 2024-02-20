0a53db1a058bb2a7af6bf51551f52959bfff1684
Руслан Ефимов <ruskus011294@gmail.com>
2024-02-20 16:05:42 +0300

    Добавил GET-запрос на получение гифок

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ API_URL = 'https://api.telegram.org/bot'
 API_CAT_URL = 'https://api.thecatapi.com/v1/images/search'
 API_DOG_URL = 'https://random.dog/woof.json'
 API_FOX_URL = ' https://randomfox.ca/floof/'
+API_GIF_URL = 'https://yesno.wtf/api?force=yes'
 BOT_TOKEN = '6460540408:AAHkYNEg2skgqOBKKhHjfAacLjMq-V4tdc0'
 ERROR_TEXT = 'Здесь должны были быть котики'
 MAX_COUNTER = 100
-api_url = [API_DOG_URL, API_FOX_URL, API_CAT_URL]
+api_url = [API_DOG_URL, API_FOX_URL, API_CAT_URL,API_GIF_URL]
 
 offset = -2
 counter = 0
@@ -36,6 +37,9 @@ while counter < MAX_COUNTER:
             elif response.status_code == 200 and random_url == API_DOG_URL:
                 link = response.json()['url']
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={link}')
+            elif response.status_code == 200 and random_url == API_GIF_URL:
+                link = response.json()['image']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendAnimation?chat_id={chat_id}&animation={link}')
             else:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
