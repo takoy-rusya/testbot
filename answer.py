a00d1a3e406209e3a68094de9b933812b177bad0
Руслан Ефимов <ruskus011294@gmail.com>
2024-02-27 13:57:13 +0300

    Добавил send_copy в ответы кроме /start, /help, /settings

--- answer.py
+++ answer.py
@@ -1,36 +1,48 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram import F
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(token=BOT_TOKEN)
+
 dp = Dispatcher()
 
 
-# Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
+@dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    """Хэндлер срабатывает на команду /start"""
+    await message.answer('Здарова, заебал. Чё надо?')
 
 
-# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['settings']))
+async def process_settings_command(message: Message):
+    """Хэндлер срабатывает на команду /settings"""
+    await message.answer("Я подскажу тебе с настройками")
+
+
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
+    """Хэндлер срабатывает на команду /help"""
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
         'я пришлю тебе твое сообщение'
     )
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+        print(message.model_dump_json(indent=1, exclude_none=True))     #Печатает в терминал JSON
+    except TypeError:
+        await message.reply(
+            text="Неподдерживаемый формат для send_copy"
+        )
+
 
 if __name__ == '__main__':
-    dp.poll_answer(bot)
+    dp.run_polling(bot)
